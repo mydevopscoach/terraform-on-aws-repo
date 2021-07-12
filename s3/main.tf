9f3fe57bb5385ad51ae55071f659f838b50f4dbc
mydevopscoach <30705206+mydevopscoach@users.noreply.github.com>
2021-07-12 11:52:11 -0500

    Update main.tf

--- s3/main.tf
+++ s3/main.tf
@@ -3,18 +3,11 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "my-s3-bucket" {
-  bucket_prefix = "my-s3bucket-"
-  acl = "private"
+  bucket_prefix = var.bucket_prefix
+  acl = var.acl
    versioning = {
-    enabled = true
+    enabled = var.versioning
   }
-  tags = {
-    Name = "My S3 bucket"
-    Environment = "POC"
-   }
-}
-
-# Create Output variable to print what is being created
-output "s3_bucket_name" {
-  value = aws_s3_bucket.my-s3-bucket.id
+  
+  tags = var.tags
 }
