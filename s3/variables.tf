1cb5aa7fdccf3f001e4cb9774c4a0457fc96cf8e
mydevopscoach <30705206+mydevopscoach@users.noreply.github.com>
2021-07-12 11:48:32 -0500

    Update variables.tf

--- s3/variables.tf
+++ s3/variables.tf
@@ -1,4 +1,26 @@
 variable "aws_region" {
-  description = "The AWS region to create things in."
+  description = "The AWS region to use to create resources."
   default     = "us-east-2"
 }
+
+variable "bucket_prefix" {
+    type        = string
+    description = "(required since we are not using 'bucket') Creates a unique bucket name beginning with the specified prefix. Conflicts with bucket."
+    default     = "my-s3bucket-"
+}
+
+variable "tags" {
+    type        = map
+    description = "(Optional) A mapping of tags to assign to the bucket."
+    default     = {
+        environment = "DEV"
+        terraform   = "true"
+    }
+}
+
+variable "versioning" {
+    type        = bool
+    description = "(Optional) A state of versioning."
+    default     = true
+}
+
