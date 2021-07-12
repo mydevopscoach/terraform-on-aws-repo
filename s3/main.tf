62e51d41700584eb3708e16af86cab134dbfc4a1
mydevopscoach <30705206+mydevopscoach@users.noreply.github.com>
2021-07-12 11:42:06 -0500

    Create main.tf

---
+++ s3/main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_s3_bucket" "my-s3-bucket" {
+  bucket_prefix = "my-s3bucket-"
+  acl = "private"
+  tags = {
+    Name = "My S3 bucket"
+    Environment = "POC"
+   }
+}
+
+# Create Output variable to print what is being created
+output "s3_bucket_name" {
+  value = aws_s3_bucket.my-s3-bucket.id
+}
